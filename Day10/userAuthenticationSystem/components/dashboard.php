1cb55800dc4ba2df2cc230ad35a31cb0103ebeaf
Kesavan-V-Sparkout <kesavan@sparkouttech.com>
2024-04-18 18:16:42 +0530

    task completed

--- Day10/userAuthenticationSystem/components/dashboard.php
+++ Day10/userAuthenticationSystem/components/dashboard.php
@@ -50,6 +50,7 @@ if(isset($_POST['logout'])){
                         if($timing["end"]){
                         echo "<br> Ended: ";
                         print_r($timing["end"]);
+                        echo "<hr>";
                     }
                     }
                   }
