8c04690bd59d935ccd12438c455d806386295115
Kesavan-V-Sparkout <kesavan@sparkouttech.com>
2024-04-22 15:30:15 +0530

    jquery basic 1

--- Day11/jQuery/script/main.js
+++ Day11/jQuery/script/main.js
@@ -52,6 +52,19 @@ $(function(){
     $("#ques").click(function(){
         $('#content').slideToggle(500);
     })
+
     
+
+    $('#nav-list a').on('click', function(event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            var hash = this.hash;            
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
 })
 
