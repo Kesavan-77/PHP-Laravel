a095741408a213d4405754a73340510495ef1818
Kesavan-V-Sparkout <kesavan@sparkouttech.com>
2024-04-22 13:58:40 +0530

    jquery basic 1

---
+++ Day11/jQuery/script/main.js
@@ -0,0 +1,57 @@
+// document.getElementById("btn-0").addEventListener("click", function() {
+//     alert("hii boss");
+// });
+
+// document.getElementById("btn-1").addEventListener("mouseenter", function() {
+//     this.style.display = "none";
+// });
+
+// document.getElementById("btn-2").addEventListener("click", function() {
+//     this.style.display = "none";
+// });
+
+// document.getElementById("btn-3").addEventListener("dblclick", function() {
+//     this.style.display = "none";
+// });
+
+// document.getElementById("navbtn").addEventListener("click", function() {
+//     var navList = document.getElementById("nav-list");
+//     if (navList.style.display === "block") {
+//         navList.style.display = "none";
+//     } else {
+//         navList.style.display = "block";
+//     }
+// });
+
+$(function(){
+
+    $("#btn-0").click(function(){
+        alert("hii boss");
+    })
+    
+    $("#btn-1").hover(function(){
+        $(this).hide();
+    })
+
+    $("#btn-2").click(function(){
+        $(this).hide();
+    })
+
+    $("#btn-3").dblclick(function(){
+        $(this).hide();
+    })
+
+    $("#navbtn").click(function(){
+        // $("#nav-list").fadeToggle(500);
+        
+        $("#nav-list").slideToggle(500,function(){
+            $('.sub-content').slideToggle(500);
+        });
+    })
+
+    $("#ques").click(function(){
+        $('#content').slideToggle(500);
+    })
+    
+})
+
